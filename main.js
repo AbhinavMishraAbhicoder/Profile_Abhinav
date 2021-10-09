9d9c6a28f40f45003906deb3d6d9070116751b24
AbhinavMishraAbhicoder <82748005+AbhinavMishraAbhicoder@users.noreply.github.com>
2021-10-09 15:12:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function python() {
+    window.alert("Python is an interpreted high-level general-purpose programming language. Its design philosophy emphasizes code readability with its use of significant indentation. Its language constructs as well as its object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.");
+}
+function Js() {
+    window.alert("JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions");
+}
+function css() {
+    window.alert("Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript. ");
+}
+function html() {
+    window.alert("The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.");
+}
